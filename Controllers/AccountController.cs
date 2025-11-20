a91fd31121de9a93dbe3ce193f5d2be0b8670a1d
Anna_Gorova <koshka20.03.19@gmail.com>
2025-11-20 23:39:56 +0200

    Додано адмін-панель з шаблону: контролери, представлення, компоненти та assets

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return RedirectToAction("LoginIn");
+        }
+        [HttpGet]
+        public IActionResult LoginIn()
+        {
+            return View("LoginIn");
+        }
+
+        [HttpGet]
+        public IActionResult RegisterIn()
+        {
+            return View("LoginIn");
+        }
+        
+        
+        
+        [HttpPost]
+        public IActionResult CheckUser()
+        {
+           throw new NotImplementedException();
+        }
+
+        public IActionResult RegisterUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
